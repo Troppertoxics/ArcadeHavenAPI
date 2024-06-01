0d33e6b942ed28b6a5da0b7361e358c53a7a4af0
noxirity <frazersearle@outlook.com>
2024-06-02 00:32:54 +0200

    make the code nice :)

--- endpoints/v1/ah/codes/get.js
+++ endpoints/v1/ah/codes/get.js
@@ -1,28 +1,40 @@
 module.exports = {
   path: "",
   method: "GET",
-  Auth: false,
+  Auth: true,
   run: async (req, res, mongo_client) => {
     try {
-      const code = req.query.code;
-      if (!code) return res.status(400).send({ error: "Missing parameters" });
+      const { code } = req.query;
+      if (!code) {
+        return sendResponse(res, 400, { error: "Missing parameters" });
+      }
 
-      const db = mongo_client.db("ArcadeHaven");
-      const collection = db.collection("codes");
+      const collection = mongo_client.db("ArcadeHaven").collection("codes");
       const code_data = await collection.findOne({ code });
+      if (!code_data) {
+        return sendResponse(res, 200, { status: "Invalid Code" });
+      }
 
-      if (!code_data) return res.status(200).send({ status: "Invalid Code" });
+      const { max_uses, expiration, uses } = code_data;
+      if (
+        (max_uses !== 0 && uses >= max_uses) ||
+        (expiration !== 0 && expiration < Date.now())
+      ) {
+        return sendResponse(res, 200, { status: "Code Expired" });
+      }
 
-      res
-        .status(200)
-        .send({
-          status: "Valid Code",
-          script: code_data.script,
-          notification: code_data.notification,
-        });
+      sendResponse(res, 200, {
+        status: "Valid Code",
+        script: code_data.script,
+        notification: code_data.notification,
+      });
     } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: "Internal server error" });
+      console.error("Error accessing the database: ", error);
+      sendResponse(res, 500, { error: "Internal server error" });
     }
   },
 };
+
+function sendResponse(res, status, content) {
+  res.status(status).send(content);
+}
