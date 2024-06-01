39feaa935d485d6b3ea1652a99e636edf0af8e4e
noxirity <frazersearle@outlook.com>
2024-06-01 20:18:14 +0200

    Add /codes/get endpoint

---
+++ endpoints/v1/ah/codes/get.js
@@ -0,0 +1,22 @@
+module.exports = {
+  path: "",
+  method: "GET",
+  Auth: false,
+  run: async (req, res, mongo_client) => {
+    try {
+      const code = req.query.code;
+      if (!code) return res.status(400).send({ error: "Missing parameters" });
+
+      const db = mongo_client.db("ArcadeHaven");
+      const collection = db.collection("codes");
+      const code_data = await collection.findOne({ code });
+
+      if (!code_data) return res.status(200).send({ status: "Invalid Code" });
+
+      res.status(200).send({ status: "Valid Code", script: code_data.script });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "Internal server error" });
+    }
+  },
+};
